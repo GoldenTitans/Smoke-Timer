2566ca60778f5e8c89c42152cfc0559d739c8f19
Alex <Alex@MacBookAir777.local>
2017-09-25 23:21:33 +0300

    Cig Count + Time Set

--- Smoke Timer/ViewController.swift
+++ Smoke Timer/ViewController.swift
@@ -8,16 +8,44 @@
 
 import UIKit
 
+
+
+
 class ViewController: UIViewController {
+    
+    @IBOutlet var Cigtxt: UILabel!
+    @IBAction func CigCount(_ sender: UIStepper) {
+        Cigtxt.text = String(sender.value)
+    }
+    
+    @IBOutlet weak var TimePicker: UIDatePicker!
+    @IBOutlet weak var Timetxt: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+       
+        
+        
+        
+        // Minimum Date
+        let formatter = DateFormatter()
+        
+        //Show Current Date
+        formatter.dateFormat = "HH:mm"
+        Timetxt.text = "They must last until:\n\(formatter.string(from: TimePicker.date))"
+        TimePicker.minimumDate = TimePicker.date
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func timeSelectedFromTimePicker(_ : AnyObject) {
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd-MMM' 'HH:mm"
+        Timetxt.text = "They must last until:\n\(formatter.string(from: TimePicker.date))"
+        
     }
 
 
